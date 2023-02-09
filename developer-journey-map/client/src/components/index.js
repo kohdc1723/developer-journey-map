275bccd80847d4e4bf90cbfdd5ac5350ba8011fb
kohdc <kohdc1723@gmail.com>
2023-02-08 16:45:52 -0800

    apply gitignore

--- developer-journey-map/client/src/components/index.js
+++
@@ -1,6 +0,0 @@
-import Stage from "./Stage";
-import Touchpoint from "./Touchpoint";
-import Column from "./Column";
-import GoalsAndNeeds from "./GoalsAndNeeds";
-
-export { Stage, Touchpoint, Column, GoalsAndNeeds };
--- developer-journey-map/client/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-import Login from "./Login";
-import Map from "./Map";
-
-export { Map, Login }
