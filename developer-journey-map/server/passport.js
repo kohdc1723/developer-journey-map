eb9019c1abf21b365255188b827ed3f7dcc9a8d7
Skylar Park <90671077+pjy990625@users.noreply.github.com>
2023-03-21 17:29:57 -0700

    removed keys

--- developer-journey-map/server/passport.js
+++ developer-journey-map/server/passport.js
@@ -4,13 +4,6 @@ import { Strategy as LinkedInStrategy } from 'passport-linkedin-oauth2';
 import passport from 'passport';
 import User from "./mongodb/models/user.js";
 
-const GOOGLE_CLIENT_ID = "799145484325-8o2poeemj5cf56i6lc0m1rb1lmfafrtm.apps.googleusercontent.com";
-const GOOGLE_CLIENT_SECRET = "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";
-const GITHUB_CLIENT_ID = "d0e34e16edc85ffb22f7";
-const GITHUB_CLIENT_SECRET = "99db574f53ea69da627911050f258eef2a304e02";
-const LINKEDIN_KEY = "86wa2tttlzoers";
-const LINKEDIN_SECRET = "i8vE4O2CXEPIN129";
-
 // check if user exists in database
 function checkUser(profile, done) {
     const userId = profile.id;
@@ -40,24 +33,24 @@ function checkUser(profile, done) {
 }
 
 passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "/auth/google/callback",
 }, function (accessToken, refreshToken, profile, done) {
     checkUser(profile, done);
 }));
 
 passport.use(new GitHubStrategy({
-    clientID: GITHUB_CLIENT_ID,
-    clientSecret: GITHUB_CLIENT_SECRET,
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
     callbackURL: "/auth/github/callback"
 }, function (accessToken, refreshToken, profile, done) {
     checkUser(profile, done);
 }));
 
 passport.use(new LinkedInStrategy({
-    clientID: LINKEDIN_KEY,
-    clientSecret: LINKEDIN_SECRET,
+    clientID: process.env.LINKEDIN_KEY,
+    clientSecret: process.env.LINKEDIN_SECRET,
     callbackURL: "/auth/linkedin/callback",
     scope: ['r_emailaddress', 'r_liteprofile'],
 }, function (token, tokenSecret, profile, done) {
