41c361f4cf51b732317a3b61724bea34b9358073
kohdc <kohdc1723@gmail.com>
2023-04-06 02:27:46 -0700

    dashboard

--- developer-journey-map/client/src/pages/Dashboard.jsx
+++ developer-journey-map/client/src/pages/Dashboard.jsx
@@ -1,80 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
-// import { confirmAlert } from "react-confirm-alert";
-import { FiMoreVertical } from "react-icons/fi";
-import { BsFillMapFill } from "react-icons/bs";
-import { AiFillPlusCircle } from "react-icons/ai";
-import { IconButton, Menu, MenuItem, Backdrop } from "@mui/material";
-// import Button from '@mui/material/Button';
-// import Menu from '@mui/material/Menu';
-// import MenuItem from '@mui/material/MenuItem';
+import { useParams, useNavigate } from "react-router-dom";
 
 import "react-confirm-alert/src/react-confirm-alert.css";
 import "../assets/styles/dashboard.css";
 import "../assets/styles/app.css";
+import defaultMapImage from "../assets/img/map.jpg";
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import { MapRow } from "../components";
 
 const Dashboard = () => {
     const { uid } = useParams();
     const [maps, setMaps] = useState([]);
     const [user, setUser] = useState(null);
-
-    const [anchorEl, setAnchorEl] = React.useState(null);
-
-    const open = Boolean(anchorEl);
-
-    const handleClick = (e) => {
-        e.preventDefault();
-        setAnchorEl(e.currentTarget);
-    };
-
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
-
-    // const handleClickDelete = (mapId) => {
-    //     confirmAlert({
-    //         message: "Are you sure to delete this map?",
-    //         buttons: [
-    //             {
-    //                 label: "Delete",
-    //                 style: {
-    //                     backgroundColor: "red"
-    //                 },
-    //                 onClick: () => deleteMap(mapId)
-    //             },
-    //             {
-    //                 label: "Cancel"
-    //             }
-    //         ]
-    //     });
-    // };
-
-    const duplicateMap = async ({ _id }) => {
-        const response = await fetch(`http://localhost:3800/api/map/duplicate/${_id}`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                uid: uid,
-                lastModified: new Date()
-            })
-        });
-
-        const duplicate = await response.json();
-        setMaps(maps.unshift(duplicate.result));
-    }
-
-    const deleteMap = async (mapId) => {
-        const response = await fetch(`http://localhost:3800/api/maps/${mapId}`, {
-            method: "DELETE"
-        });
-
-        const deletedMap = await response.json();
-        setMaps(maps.filter(map => map._id !== deletedMap.result._id));
-    }
+    const navigate = useNavigate();
 
     const createMap = async (uid) => {
         const response = await fetch(`http://localhost:3800/api/map/${uid}`, {
@@ -82,9 +21,16 @@ const Dashboard = () => {
         });
 
         const createdMap = await response.json();
-        console.log("c: " + createdMap.data._id)
-        setMaps(maps.unshift(createdMap.data));
-    }
+        setMaps([...maps, createdMap.data]);
+
+        return createdMap.data;
+    };
+
+    const handleCreate = async (uid) => {
+        const createdMap = await createMap(uid);
+        console.log(createdMap)
+        navigate(`/map/${createdMap._id}`);
+    };
 
     useEffect(() => {
         const getUser = () => {
@@ -123,7 +69,22 @@ const Dashboard = () => {
     return (
         <React.Fragment>
             <Navbar user={user} />
+
             <div id="dashboard" className="main dashboard">
+                <h2 className="create-title">Start a new map</h2>
+                <div className="create-container">
+                    <div
+                        className="create-template"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            handleCreate(uid);
+                        }}
+                    >
+                        <img src={defaultMapImage} alt="default map" width={250} />
+                        <div className="create-subtitle">Create a default map</div>
+                    </div>
+                </div>
+
                 <div className="label">
                     <div></div>
                     <div className="title-column">Title</div>
@@ -131,94 +92,10 @@ const Dashboard = () => {
                     <div></div>
                 </div>
 
-                <div className="create-row">
-                    <AiFillPlusCircle
-                        className="create-button"
-                        onClick={(e) => {
-                            e.preventDefault();
-                            createMap(uid);
-                        }}
-                    />
-                </div>
-
                 {maps
                     .sort((a, b) => new Date(b.lastModified) - new Date(a.lastModified))
                     .map((map, index) => (
-                        <div key={index}>
-                            <Link to={`/map/${map._id}`} className="map-item">
-                                <div className="grid-container">
-                                    <div>
-                                        <BsFillMapFill className="map-icon" />
-                                    </div>
-                                    <div className="title-column">
-                                        {map.title}
-                                    </div>
-                                    <div className="last-modified-column">
-                                        {new Date(map.lastModified).toLocaleString()}
-                                    </div>
-                                    <div className="more">
-                                        <IconButton
-                                            className="more-icon-cover"
-                                            aria-label="more"
-                                            aria-controls={open ? 'basic-menu' : undefined}
-                                            aria-haspopup="true"
-                                            aria-expanded={open ? 'true' : undefined}
-                                            onClick={handleClick}
-                                        >
-                                            <FiMoreVertical />
-                                        </IconButton>
-                                        <Menu
-                                            // id="basic-menu"
-                                            anchorEl={anchorEl}
-                                            open={open}
-                                            onClose={handleClose}
-                                            MenuListProps={{
-                                                'aria-labelledby': 'basic-button',
-                                            }}
-                                            onBackdropClick={handleClose}
-                                        >
-                                            <MenuItem onClick={(e) => {
-                                                e.preventDefault();
-                                                duplicateMap(map);
-                                                setAnchorEl(null);
-                                            }}>
-                                                Duplicate
-                                            </MenuItem>
-                                            <MenuItem onClick={(e) => {
-                                                e.preventDefault();
-                                                console.log(map._id)
-                                                deleteMap(map._id);
-                                                setAnchorEl(null);
-                                            }}>
-                                                Delete
-                                            </MenuItem>
-                                        </Menu>
-                                    </div>
-                                    {/* <div style={{display: "flex"}}>
-                                        <button
-                                            className="button"
-                                            type="button"
-                                            onClick={(e) => {
-                                                e.preventDefault();
-                                                duplicateMap(map);
-                                            }}
-                                        >
-                                            Duplicate
-                                        </button>
-                                        <button
-                                            className="button"
-                                            type="button"
-                                            onClick={(e) => {
-                                                e.preventDefault();
-                                                handleClickDelete(map._id);
-                                            }}
-                                        >
-                                            Delete
-                                        </button>
-                                    </div> */}
-                                </div>
-                            </Link>
-                        </div>
+                        <MapRow key={map._id} map={map} uid={uid} maps={maps} setMaps={setMaps} />
                     ))
                 }
             </div>
