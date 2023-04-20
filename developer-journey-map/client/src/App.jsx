6f443005a0f869062fd227daa00d2453a8d63142
kohdc <kohdc1723@gmail.com>
2023-04-19 20:02:54 -0700

    fix

--- developer-journey-map/client/src/App.jsx
+++ developer-journey-map/client/src/App.jsx
@@ -9,24 +9,32 @@ const App = () => {
     const [user, setUser] = useState(null);
 
     useEffect(() => {
-        const getUser = () => {
-            fetch(`${process.env.REACT_APP_API_ENDPOINT}/auth/login/success`, {
-                method: "GET",
-                credentials: "include",
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                    "Access-Control-Allow-Credentials": true,
-                },
-            }).then((response) => {
-                if (response.status === 200) return response.json();
-                throw new Error("authentication has been failed!");
-            }).then((resObject) => {
-                setUser(resObject.user);
-            }).catch((err) => {
+        const getUser = async () => {
+            try {
+                const url = `${process.env.REACT_APP_API_ENDPOINT}/auth/login/success`;
+                const { data } = await fetch(url, { withCredentials: true });
+                setUser(data.user._json);
+            } catch (err) {
                 console.log(err);
-            });
+            }
+            // fetch(`${process.env.REACT_APP_API_ENDPOINT}/auth/login/success`, {
+            //     method: "GET",
+            //     credentials: "include",
+            //     headers: {
+            //         Accept: "application/json",
+            //         "Content-Type": "application/json",
+            //         "Access-Control-Allow-Credentials": true,
+            //     },
+            // }).then((response) => {
+            //     if (response.status === 200) return response.json();
+            //     throw new Error("authentication has been failed!");
+            // }).then((resObject) => {
+            //     setUser(resObject.user);
+            // }).catch((err) => {
+            //     console.log(err);
+            // });
         };
+        
         getUser();
     }, []);
 
