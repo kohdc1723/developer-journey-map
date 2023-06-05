a80bebc1be295a7aeaedadbc48146f2997f92c6f
kohdc <kohdc1723@gmail.com>
2023-06-05 00:59:50 -0700

    fix cors

--- server/index.js
+++ server/index.js
@@ -12,12 +12,11 @@ dotenv.config();
 
 const app = express();
 
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(cookieSession({ name: "session", keys: ["revere"], maxAge: 24 * 60 * 60 * 1000 }));  // 24 hours
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: `${process.env.CLIENT_URL}`, methods: "GET,POST,PUT,DELETE", credentials: true }));
+app.use(cors());
 
 app.use("/api/map", mapRoutes);
 app.use("/api/maps", mapsRoutes);
